7f59009e62f5600f3c5c8f3fca5064476d077ad6
devanshihub <devanshimehrotra2024@gmail.com>
2024-07-19 12:25:43 +0530

    1 commit

---
+++ linklistbasics1.java
@@ -0,0 +1,20 @@
+package data_structures_java;
+
+class a
+{
+    int x,y;
+}
+
+public class linklistbasics1 
+{
+    public static void main(String k[])
+    {
+        a ob1=new a();
+        a ob2=new a();
+        System.out.println(ob1.x);
+        System.out.println(ob1.y);
+        System.out.println(ob2.x);
+        System.out.println(ob2.y);
+
+    }
+}
